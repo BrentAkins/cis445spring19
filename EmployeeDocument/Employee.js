e9b7984d8d8330138bb02cc12357f2a407131f04
Brent Akins <bakins@una.edu>
2019-02-20 01:02:23 +0000

    Employees V1.00

---
+++ EmployeeDocument/Employee.js
@@ -0,0 +1,47 @@
+          {
+            "employees": "<emp_no>",
+            "employees": [
+               {
+                "emp_number": 00012
+                "birth_date": "02/03/55"
+                "first_name": "John",
+                "last_name": "Doe"
+                "gender":"M"
+                "hire_date": "01/01/79"
+                "salaries": [{"emp_number": 00012,
+                             "salary": 79000,
+                             "from_date": "02/01/79",
+                             "to_date": "06/01/79"}],
+                "titles": [{"emp_number": 00012,
+                            "title": "chief engineer",
+                            "from_date": "01/01/79",
+                            "to_date": "06/01/79"}]
+                "dept_emp":[{"emp_no": 00012,
+                             "dept_no": "23E",
+                             "from_date": "01/01/79",
+                             "to_date": "06/01/79"}]
+                }
+            ]
+            
+            "dept_manager": "<emp_no>",
+            "dept_manager": [
+                {
+                 "dept_no": "23E",
+                 "emp_no": 00012,
+                 "from_date": "01/01/79",
+                 "to_date": "12/31/79"
+                }
+            ]
+            
+            "departments": "<dept_no>",
+            "deparments": [
+                {
+                 "dept_no": "23E",
+                 "dept_name": "Engineering",
+                 "dept_emp":[{"emp_no": 00012,
+                             "dept_no": "23E",
+                             "from_date": "01/01/79",
+                             "to_date": "06/01/79"}]
+                }
+            ]
+          }
